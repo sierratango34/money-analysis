d88a90de02f21dcfe60f6c28b9bdae45a610753b
sierratango34 <k.sritawan@gmail.com>
2021-02-08 22:17:33 +0000

    Ref T4 - settings.py and .env ile adjustments

--- moneyanalysis/settings.py
+++ moneyanalysis/settings.py
@@ -2,18 +2,14 @@ import os
 from pathlib import Path
 import environ
 
-env = environ.Env(
-    DEBUG=(bool, False)
-)
-environ.Env.read_env()
-
-BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+env.read_env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -64,8 +60,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'moneyanalysis',
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'USER': env.read_env('POSTGRES_USER'),
+        'PASSWORD': env.read_env('POSTGRES_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
