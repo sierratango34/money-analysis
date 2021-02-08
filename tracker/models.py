7d8b3007e5cd8f8a14f059a11c0bd6f108e93fc0
sierratango34 <k.sritawan@gmail.com>
2021-02-08 22:44:45 +0000

    Ref T3 - initial commit of sheet and transaction models

---
+++ tracker/models.py
@@ -0,0 +1,59 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Sheet(models.Model):
+    title = models.CharField(max_length=200)
+    start_date = models.DateTimeField('start date')
+    end_date = models.DateTimeField('end date')
+
+    def __str__(self):
+        return self.title
+    
+
+class Transaction(models.Model):
+    GENERAL = 'general'
+    GROCERIES = 'groceries'
+    SHOPPING = 'shopping'
+    ENTERTAINMENT = 'entertainment'
+    EATING_OUT = 'eating_out'
+    PERSONAL_CARE = 'personal_care'
+    CHARITY = 'charity'
+    SAVINGS = 'savings'
+    GIFTS = 'gifts'
+    TRANSPORT = 'transport'
+    HOLIDAY = 'holiday'
+    FAMILY = 'family'
+    DEBT = 'debt'
+    BILLS = 'bills'
+    FINANCES = 'finances'
+    FUEL = 'fuel'
+
+    CATEGORY_CHOICES = [
+        (GENERAL, 'General'),
+        (GROCERIES, 'Groceries'),
+        (SHOPPING, 'Shopping'),
+        (ENTERTAINMENT, 'Entertainment'),
+        (EATING_OUT, 'Eating Out'),
+        (PERSONAL_CARE, 'Personal Care'),
+        (CHARITY, 'Charity'),
+        (SAVINGS, 'Savings'),
+        (GIFTS, 'Gifts'),
+        (TRANSPORT, 'Transport'),
+        (HOLIDAY, 'Holiday'),
+        (FAMILY, 'Family'),
+        (DEBT, 'Debt'),
+        (BILLS, 'Bills'),
+        (FINANCES, 'Finances'),
+        (FUEL, 'Fuel'),
+    ]
+
+    sheet = models.ForeignKey(Sheet, on_delete=model.CASCADE)
+    description = models.CharField(max_length=200)
+    category = models.CharField(max_length=30, choices=CATEGORY_CHOICES, default=GENERAL)
+    amount = models.IntegerField(default=0)
+    date = models.DateField('date')
+
+
+    def __str__(self):
+        return self.description
