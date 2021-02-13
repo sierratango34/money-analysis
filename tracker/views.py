1a464b3a800fa6aab71b104f4bcd9e872b63a32d
sierratango34 <k.sritawan@gmail.com>
2021-02-13 22:27:26 +0000

    Ref T7 - WIP: add more to views

--- tracker/views.py
+++ tracker/views.py
@@ -17,8 +17,27 @@ class SheetDetailView(generic.DetailView):
     model = Sheet
     template_name = "tracker/sheet-detail.html"
 
+
+class NewTransactionDetailView(generic.DetailView):
+    model = Transaction
+    template_name = "tracker/new-transaction-form.html"
+
 # class TransactionDetailView(generic.DetailView):
 #     model = Transaction
 #     template_name = "tracker/transaction-detail.html"
 
+def addTransaction(request, sheet_id):
+    sheet = get_object_or_404(Sheet, pk=sheet_id)
+    try:
+        transaction = sheet.transaction_set.get(pk=request.POST['transaction'])
+    except (KeyError, Sheet.DoesNotExists):
+        # Redisplay the sheet
+        return render(request, 'tracker/sheet-detail.html', {
+            'sheet': sheet,
+            'error_message': "Incomplete transaction details",
+        })
+    else:
+        transaction.save()
+
+    return HttpResponseRedirect(reverse('tracker:addTransaction', args=(sheet.id,)))
 
