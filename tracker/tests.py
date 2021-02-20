de6b55ead68e3017002769d5799bc6e8260d08c3
sierratango34 <k.sritawan@gmail.com>
2021-02-20 19:59:19 +0000

    Ref T12 - initial test for no sheets

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Sheet, Transaction
 
-# Create your tests here.
+
+class SheetIndexViewTests(TestCase):
+    def test_no_sheets(self):
+        """
+        If no sheets exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('tracker:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No sheets are available for viewing.")
+        self.assertQuerysetEqual(response.context['sheet_list'], [])
